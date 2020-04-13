3af860e4fef798e784e0049922b45c217a900b9f
HrushikeshReddy <44958633+HrushikeshReddy@users.noreply.github.com>
2020-04-13 12:09:00 +0530

    Add files via upload

---
+++ Capstone_Project.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ASSIGNMENT-
+# 
+# 1.Write some markdown to explain that this notebook will be mainly used for the capstone project.
+# 
+# 2.Import the pandas library as pd.
+# 
+# 3.Import the Numpy library as np.
+# 
+# 4.Print the following the statement: Hello Capstone Project Course!
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
